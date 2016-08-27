e56a47c1204fec85e8dea83b26d810ed5c1f9be6
colleenmcginnis <colleen.j.mcginnis@gmail.com>
2016-08-27 15:19:23 -0700

    clean up code for project gallery

---
+++ data/gallery.js
@@ -0,0 +1,59 @@
+var galleryList = {
+	"id": "gallery",
+	"images": [
+  		{
+  		"caption": "Planter Box",
+  		"price": "$60",
+  		"status": "<a href='https://www.etsy.com/shop/RandomDoor'>available now on Etsy</a>",
+		"src": "https://lh3.googleusercontent.com/0to_7AkkG1G-QOOaxmNIiHF_bDYxmXvRVuI4mSe0glKN0gU_NgOPJJOLYhk_sHkSSAEBhPjuSURkUZ-lThV89VsbLvWvgv3972jr7GXPlq5Z2H9dFwpSKDPbEG33ikyZ6lDC6oLqBNczvJXMS0VXljfDqJqsTTUn_haFnMdP68JDAjz1RsJywOZw4qjdBDk0QB93LAUpJdbi1IOEQiSeHIvOb3FZs_IfM3VzrYEPOh2C_YUA-UKTPnY5RW2d8xrhj9usyTBIUxHYUsXkZOJqd5iVisRNIILq--iOeh4KLYu8o4oliXP2FLrkM8EgPDsAPeEI2-6TalD3gByE6ZbL-uc9rjKOMNhxOdIKDhK5CXPp0rNKuF64uLrbmHfWTvctHxIlsiAZK3xj20M0-TvMZUpXTVkCOwJJ0sIvxSsPscljVZY804XRXLbhPF7u7fQLhuQlR1xJEMcgUQxOcxVWB3g-UCqj6BmcBCUuBKV9M2ewpEAlHQMRzlMT7loyOySSKoorSDiGIFHLppz3TAz2k1ej6dtf8pO3uGfkm6Jg6ToA0W5xij8jWhvDfj0t8E-QArSxuLM1v-LR4uSFG8qd2mSpMJ8BiS5ttKIbjAGvJW5b7Led=w1055-h703-no",
+  		},
+  		{
+  		"caption": "Cutting Boards",
+  		"price": "$60",
+  		"status": "<a href='https://www.etsy.com/shop/RandomDoor'>available now on Etsy</a>",
+		"src": "https://lh3.googleusercontent.com/1WVjDXjm183090nlik_4NqD5hNplgsjhomquTT6eysF9OBp_YUndwPrNGclBrlRZntCIhBB_7idQH6uHdhvWM-crUUH6LyqTFGg-R5w21MFroxoT4dtv6mRQRf8eG7O6OpEHixDg4EFmJIuOdxyZIIHmG559lTsF__8AJ5Ppndr28wHVG9ONGD6C0h2RRwZi7tTN3Hh53v8y6UDtIaVeoBAakviGEXhjk1iVYP153QKjrrcd6aL-Fvt6UTfk66V3CQT0AAdOtZ74POrWuL7MRFwzN2qSKptvQwL57K-ZVLj-cijD9fZ8IYsJnMpKtSEV9XN1wxMUFZFRrimGTG88E8ii1rYCq9fjnf2gwyPW_iBQEM6vP18wTm-skpAQhqSGI_JYIk9BHWplAQBIMdIU0pRXYRewJ8JuXBfYqUpsuYSJbGua2_B0yEvSTtDAY0XwNNOVzLg8BF7d0nodI-Ypd8vck4iesXkdt8pX7eeprZRfHRKWT1qchNmYDqtRVVVy75a5cwBMTY9BM7hmn3jWcGYS7-_XHmRKUzfMwq9M1NFCN1PoRT92gN5vc0l7tz1fR8QGdO_qwqGriCXWepciyMy5gUGO-tPAQNhIJdHbGChktPlw9Q=w1055-h703-no",
+  		},
+  		{
+  		"caption": "Coffee Table",
+  		"price": "price varies",
+  		"status": "<a href='#contact'>made to order</a>",
+		"src": "img/coffee_carousel.jpg",
+  		},
+  		{
+  		"caption": "Side table",
+  		"price": "price varies",
+  		"status": "<a href='#contact'>made to order</a>",
+		"src": "https://lh3.googleusercontent.com/x0C7kRnoF1cWwgf40QUFytARUmj1G3B9DkfAGqPBLO_o0_kjahle3-_QpaSDGJsja1jW62v4B3HYI3GKED-Yj0orBLKEJ56nX1RsDA4hj5vsfCqZMKjx5j9Se-sqM_1V5jOTknnvdAqBfTEo13qM54e3YyVoMsmKgvicWeZH56UNHV9u9hUgMN7I9KelTu07Zr_phIVAWV6BFd7nI4b5UnsAmZAElj_elQHrvF3n4go6mZD1GQQvuOK5ZpS6dCNKTbh32ZK5u1JcFa2duhL50eQ-4v3BtdyXQ5Jj-DI0qM7RBGCIO2W4jExLmQDjdCdwz6gEWN1C99vk73H3JvrtTgl3KkcLOkOMzgPIU058vDLXIdkI9arpwuAp6b-yHPJKSUAYKK8IxjOtLZTyowSK-sezEU2P_0mLzzRKsgNy2vf4F0o6NYjWrTF1R5PSTYittkOkvY46DoL51RftQRxRY_OW88BfIo-4V6n3fktEZU4M4e7ELOCVmHliEFQLek5u6KUEATRvbOqNxIrhebB_wVRj9O-Zwg0kX168D_vXd4EFfcMpuPUNOKa1MUN7ZdSB1mJRrcwyLO6kDJkJh03PEQDw4D8tHdcMgN8Ely6ONP6X4fEh=w989-h659-no",
+  		},
+  		{
+  		"caption": "Wood Butter",
+  		"price": "$20-25",
+  		"status": "<a href='https://www.etsy.com/shop/RandomDoor'>available now on Etsy</a>",
+		"src": "img/wood-butter.jpg",
+  		},
+  		{
+  		"caption": "Desk",
+  		"price": "price varies",
+  		"status": "<a href='#contact'>made to order</a>",
+		"src": "https://lh3.googleusercontent.com/4cLapMpBb03jqygcZyo9v19V4fUwykiXr7gS666Mno3o-sOUyRxYs0MdMWpR9bRhfBpXoDmYWRScpOK9YxIi-xx3PR3T2_XH-fagubkXwaKNfxQ6vnLRCc0ZF9czJENDn_xm6OU1w4zvIcRA-u9miK2YFlNBoWXOV82-bHBnC-7rJoIiyx0APzptrGlZ9dfiGj9W9ubvmbZ67WV5aam1nVLDmff6PCD7eklmJ-3Kp8w3o-JRs_-mmRE2sm7gG3P4neelZ0BLcVgoskIxeZ7pCVSqK66Z_VNH7e_IxZ2iEWt4pDDKDjJR0sZY4Ktj19993rTqROLjP6vjgzziT-lbr5AEucv8DJEjSev9qkhohsSs90D3Ufn9ZcdNY9mvJCVi8hxa_wcztijRbUFnQAzbxPHa7zed-pEgCLOGJShE9P6ma0GIxSd-lCZxuTUDHvBq4v-J5ZqqOLfTz4kVaWOF5PVMhmMudj0bkyki1C5U_iO-fEaKmb6EgGVlhAQuxnKbMYGbSDrJpcLOJNv5HhC2Afs4qbaIED3gFFY_hKEGBU8NN9sjfIf59fSPzeSjjMCzfjkEcrCKIkdQ7_pULeJdf-MUW50kOY6r9AyK0a1EkYh98Ga2=w989-h659-no",
+  		},
+  		{
+  		"caption": "Kitchen Table",
+  		"price": "price varies",
+  		"status": "<a href='#contact'>made to order</a>",
+		"src": "img/table_carousel.jpg",
+  		},
+  		{
+  		"caption": "Picture Frame",
+  		"price": "price varies",
+  		"status": "<a href='#contact'>made to order</a>",
+		"src": "https://lh3.googleusercontent.com/BxZ9g5zC6_TDaw4KI4-xFaU1DSXgSpEg9DKgmpL7-AuSOXqcxOBOJYZ1yFpPkvBzbVv4G0N5yr06GgsRp2q4cXy2ChaEiChxU_7o9rGCBf8HpKRajh4tWAoYFbyu6xXtR9e6A8oDTEpm5avtelbfIUgu1tQ0b9it5yRN4CB5kpt2LfurNToLtfd-hoC7m6B6hPYpi0EC-01fTtSJJ0iVBBszXMhU5vRpZ2hA4xxv08plx4-n3i-4DPY4-GsXJrWAQb4pm3lznc-hEmq8xVZyWBhKQyjCf7gdlVmttTdzD_7n6_lYERQzzpYXPRgv8oAabhOPAMlrCK0XW6cR2Kdoei4zBJyl99Bm__yKHb-DAS_8zccFD16dqLxPdGwPUUshazrGW7eQZsxxAQMkgN6OADCQ_U_nma0bmozVKdkMhccNSrsgvMS2MX3LTIj5fIi2assdyHbHzYGc0oMZOycBJSEohX6iBLecyOXb8Lvgt4G7L7U9jZZzXWlA96zCPEHLRIiWQ18MfFTGJAf0NKpGgtoYfaKSPJ8Qb6ehDInUkejR-HOEBCbteT1mAK5aoXZcxYufLwSV312tSAQXX-Aob7xpy2HsR1l4=w1055-h703-no",
+  		},
+  		{
+  		"caption": "Coasters",
+  		"price": "$12",
+  		"status": "<a href='https://www.etsy.com/shop/RandomDoor'>available now on Etsy</a>",
+		"src": "https://lh3.googleusercontent.com/FQ7C6TLbI6UweoVRbHcXAyqF8nqeVhoUvl85SBuE3WDt-Gznb0LCDfy7ckMeQ7soUtsWbukfztoG1izuyfSyp5JIn1mgvS-u9ca_nug23IhlF3x3AJUAgv6K7bl27gsIOWxdwB9qjEZyeGRGyxPmIIxTWCG0AweFflXXbMe6IoGr2WHUzAmA1Df5x_hi8r9a1WqnN7Oeu2jCccrsC0EnsKzJqq-aHH0ndQ-gCFucy3TYlsIe9sky9ultP23ZhiimxfZ-15tUucxo9Fvu1oxYhoZzwYjwqoLZYFrudfTAJOIztEzKwkIZCOSFIIrdR-I7of3mqbtXQ2Rx_7xG7A6opf6OmoSbHV0ZHWId_PNjAVSPOM7KWsqU_tgExoXODDHrjXh96OG1iRQ0KUwGl3zK5G0QN4O91GL4j4ClCZDnqZ_cM33GXlNmQMHMmROkpitAS_2t1cQayjaCMVNKhBRz9te9GpkAOKMQEYlkB3AyYQwUa2AhcjXeyYt-xEnENRRZTQvZS5Emug9q9wxoCBGWsUG84bPSLK9MA18UHO6ZwQN0DrR83ZCmIQhw6_o3Uy_AYWiPPybE1QLvE3Dqb3qTJgp_8mFuetabf3hsLYiZnmS1rUnY=w1055-h703-no",
+  		}
+  	]
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //Smooth Scroll
-
 $('nav a').on('click', function() {
   // use the href of the link to identify what
   // section to scroll to
@@ -25,10 +24,7 @@ $('.fa-chevron-down').on('click', function() {
 });
 
 
-
-
 //Nav properties
-
 var navPosition = $('.container').offset().top;
 var navPosition = navPosition - 60;
 
@@ -52,79 +48,49 @@ $('.hamburger').on('click', function(){
 });
 
 
-//Carousel
-
-var images=[
-
-'https://lh3.googleusercontent.com/0to_7AkkG1G-QOOaxmNIiHF_bDYxmXvRVuI4mSe0glKN0gU_NgOPJJOLYhk_sHkSSAEBhPjuSURkUZ-lThV89VsbLvWvgv3972jr7GXPlq5Z2H9dFwpSKDPbEG33ikyZ6lDC6oLqBNczvJXMS0VXljfDqJqsTTUn_haFnMdP68JDAjz1RsJywOZw4qjdBDk0QB93LAUpJdbi1IOEQiSeHIvOb3FZs_IfM3VzrYEPOh2C_YUA-UKTPnY5RW2d8xrhj9usyTBIUxHYUsXkZOJqd5iVisRNIILq--iOeh4KLYu8o4oliXP2FLrkM8EgPDsAPeEI2-6TalD3gByE6ZbL-uc9rjKOMNhxOdIKDhK5CXPp0rNKuF64uLrbmHfWTvctHxIlsiAZK3xj20M0-TvMZUpXTVkCOwJJ0sIvxSsPscljVZY804XRXLbhPF7u7fQLhuQlR1xJEMcgUQxOcxVWB3g-UCqj6BmcBCUuBKV9M2ewpEAlHQMRzlMT7loyOySSKoorSDiGIFHLppz3TAz2k1ej6dtf8pO3uGfkm6Jg6ToA0W5xij8jWhvDfj0t8E-QArSxuLM1v-LR4uSFG8qd2mSpMJ8BiS5ttKIbjAGvJW5b7Led=w1055-h703-no',
-
-'https://lh3.googleusercontent.com/1WVjDXjm183090nlik_4NqD5hNplgsjhomquTT6eysF9OBp_YUndwPrNGclBrlRZntCIhBB_7idQH6uHdhvWM-crUUH6LyqTFGg-R5w21MFroxoT4dtv6mRQRf8eG7O6OpEHixDg4EFmJIuOdxyZIIHmG559lTsF__8AJ5Ppndr28wHVG9ONGD6C0h2RRwZi7tTN3Hh53v8y6UDtIaVeoBAakviGEXhjk1iVYP153QKjrrcd6aL-Fvt6UTfk66V3CQT0AAdOtZ74POrWuL7MRFwzN2qSKptvQwL57K-ZVLj-cijD9fZ8IYsJnMpKtSEV9XN1wxMUFZFRrimGTG88E8ii1rYCq9fjnf2gwyPW_iBQEM6vP18wTm-skpAQhqSGI_JYIk9BHWplAQBIMdIU0pRXYRewJ8JuXBfYqUpsuYSJbGua2_B0yEvSTtDAY0XwNNOVzLg8BF7d0nodI-Ypd8vck4iesXkdt8pX7eeprZRfHRKWT1qchNmYDqtRVVVy75a5cwBMTY9BM7hmn3jWcGYS7-_XHmRKUzfMwq9M1NFCN1PoRT92gN5vc0l7tz1fR8QGdO_qwqGriCXWepciyMy5gUGO-tPAQNhIJdHbGChktPlw9Q=w1055-h703-no', 
-
- 'img/coffee_carousel.jpg', 
-
- 'img/tableTwo_carousel.jpg', 
-
- 'img/desk_carousel.jpg', 
-
- 'img/table_carousel.jpg', 
-
- 'https://lh3.googleusercontent.com/BxZ9g5zC6_TDaw4KI4-xFaU1DSXgSpEg9DKgmpL7-AuSOXqcxOBOJYZ1yFpPkvBzbVv4G0N5yr06GgsRp2q4cXy2ChaEiChxU_7o9rGCBf8HpKRajh4tWAoYFbyu6xXtR9e6A8oDTEpm5avtelbfIUgu1tQ0b9it5yRN4CB5kpt2LfurNToLtfd-hoC7m6B6hPYpi0EC-01fTtSJJ0iVBBszXMhU5vRpZ2hA4xxv08plx4-n3i-4DPY4-GsXJrWAQb4pm3lznc-hEmq8xVZyWBhKQyjCf7gdlVmttTdzD_7n6_lYERQzzpYXPRgv8oAabhOPAMlrCK0XW6cR2Kdoei4zBJyl99Bm__yKHb-DAS_8zccFD16dqLxPdGwPUUshazrGW7eQZsxxAQMkgN6OADCQ_U_nma0bmozVKdkMhccNSrsgvMS2MX3LTIj5fIi2assdyHbHzYGc0oMZOycBJSEohX6iBLecyOXb8Lvgt4G7L7U9jZZzXWlA96zCPEHLRIiWQ18MfFTGJAf0NKpGgtoYfaKSPJ8Qb6ehDInUkejR-HOEBCbteT1mAK5aoXZcxYufLwSV312tSAQXX-Aob7xpy2HsR1l4=w1055-h703-no', 
-
- 'https://lh3.googleusercontent.com/6uSdfTaRvQ7R5xmPCr1wt6ie63t7wgHqBMZ6xKR5ZCAX4kxqcR4_lV729srljDcdAY50VWlqjibDLkVbZRblWnswcrQqgTYH28CB-HWvpAAJacIi3RpGdpCKGlLndrheXLUCK9emmcu1J_9bTXs8MlkEvF070lHv0kNy0Cu4FpxzH2DOjScLF0dJZBW7UZyItlzOzFv-v3mQ4ErKTKK4Be8ZtRztmemaX7UFt7kAWfOZnZvL1zZDunkLFO9htuKa-q2ZubHLO79pHemqWy69bFjJFvJRrJpFB1WftjoG-mHpKwnQDjehqDMYuxLhoLlOedwI1Cnv7BO4r710tuTMwwzwdZfbRy--ICzZUMYugePafcIyN6JgCbj-jkfhjdY7ThOrN9BtBraOLmP1CrxT0l0sUtjqJGPBJkuErvlHs6DUET_qV-kWon2du-7R36toayEEg67bQiW7UTWltp3hMVBzP0QEu56WeM4dwlJU_D6-uv0EwZLHfp-r0aq1RbZhxZYZZqjXQLZw1R8nREGFcKzV22NQNnC7SuhhE9neQhiExeppZ6_ebgIKAt-IV70o-QDY2i9zzyPTk6UCx4aHLO2zcOiP__k=w1212-h682-no'];
-// var countDots=['#count-one', '#count-two', '#count-three', '#count-four']
-// var currentPosition = 0;
+// Gallery
+var images = galleryList.images;
 
 window.onload = function(){
 	for (i=0; i < images.length; i++){
 		var gallery = document.getElementById('gallery');
+		// Create thumbnail images
 		var project = gallery.appendChild(document.createElement('div'));
 	  	project.className = 'project';
 	    project.id = "project-" + i;
-	  	project.style.backgroundImage = 'url("' + images[i] + '")';
-	  	window.onresize = function resizeit() {
-	  		var projects = document.getElementsByClassName('project');
+	  	project.style.backgroundImage = 'url("' + images[i].src + '")';
+	  	// Create captions
+	  	var caption = project.appendChild(document.createElement('div'));
+	  	caption.className = 'caption';
+	  	var title = caption.appendChild(document.createElement('h4'));
+	  	title.innerHTML = images[i].caption;
+	  	title.className = 'project-title'
+	  	var cost = caption.appendChild(document.createElement('p'));
+	  	cost.innerHTML = images[i].price;
+	  	cost.className = 'project-cost';
+	  	var availability = caption.appendChild(document.createElement('p'));
+	  	availability.innerHTML = images[i].status;
+	  	availability.className = 'project-availability';
+	  	// Keep square proportions for any screen size
+	  	var projects = document.getElementsByClassName('project');
+	  	window.onresize = function resizeit() { 		
 	  		var width = project.offsetWidth;
-		  	console.log(width);
 	  		for (i=0; i < projects.length; i++){
 		  		projects[i].style.height = width+'px';
 		  	}
+		  	caption.width = width + 'px';
 	  	}
+		project.addEventListener('mouseover', function(){
+			var currentProject = document.getElementById(this.id);
+			var currentCaption = currentProject.querySelector('.caption');
+			if (currentCaption.classList)
+			  currentCaption.classList.add('show');
+		});
+		project.addEventListener('mouseout', function(){
+			var currentProject = document.getElementById(this.id);
+			var currentCaption = currentProject.querySelector('.caption');
+			if (currentCaption.classList)
+			  currentCaption.classList.remove('show');
+		});
 	}
 };
-
-// $('#next').on('click', nextImage);
-// $('#prev').on('click', previousImage);
-
-// function nextImage(){
-// 	currentPosition++;
-
-// 	$('#prev').css('display', 'inline');
-
-// 	changeImage();
-
-//  	if (currentPosition === 3) {
-// 		$('#next').css('display', 'none');
-// 	}
-// }
-
-// function previousImage(){
-
-// 	currentPosition--;
-
-// 	$('#next').css('display', 'inline');
-
-// 	changeImage();
-
-// 	if (currentPosition === 0 ) {
-// 		$('#prev').css('display', 'none');
-// 	}
-// }
-
-// function changeImage(){
-// 	$('#carousel-image').attr('src', images[currentPosition]);
-// 	$('#count-one').css('background-color', 'rgba(255, 255, 255, 0)');
-// 	$('#count-two').css('background-color', 'rgba(255, 255, 255, 0)');
-// 	$('#count-three').css('background-color', 'rgba(255, 255, 255, 0)');
-// 	$('#count-four').css('background-color', 'rgba(255, 255, 255, 0)');
-// 	$(countDots[currentPosition]).css('background-color', 'rgba(255, 255, 255, .4)');
-// }
